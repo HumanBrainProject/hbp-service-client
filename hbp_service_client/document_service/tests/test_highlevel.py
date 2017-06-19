0e2f4bef02f3dad19864ae6c9e5b85b720401f86
Allan Francani <allan.francani@epfl.ch>
2017-06-19 17:17:43 +0200

    Test `ls` returns the files and folders of a project (with a leading slash)

--- hbp_service_client/document_service/tests/test_highlevel.py
+++ hbp_service_client/document_service/tests/test_highlevel.py
@@ -54,3 +54,47 @@ class TestStorageClient(unittest.TestCase):
 
         # then
         assert_that(file_names, equal_to([]))
+
+
+    def test_ls_should_return_the_files_of_the_project(self):
+        # given
+        self.register_uri(
+            'https://document/service/entity/?path=my_project',
+            returns={'uuid': 'e2c25c1b-f6a9-4cf6-b8d2-271e628a9a56'}
+        )
+        self.register_uri(
+            'https://document/service/folder/e2c25c1b-f6a9-4cf6-b8d2-271e628a9a56/children/?page=1&page_size=100&entity_type=file',
+            returns={'next': None, 'results': [{'name': 'file1'}, {'name': 'file2'}]}
+        )
+        self.register_uri(
+            'https://document/service/folder/e2c25c1b-f6a9-4cf6-b8d2-271e628a9a56/children/?page=1&page_size=100&entity_type=folder',
+            returns={'next': None, 'results': []}
+        )
+
+        # when
+        file_names = self.client.ls('my_project')
+
+        # then
+        assert_that(file_names, equal_to(['file1', 'file2']))
+
+
+    def test_ls_should_return_the_folders_of_the_project_with_a_leading_slash(self):
+        # given
+        self.register_uri(
+            'https://document/service/entity/?path=my_project',
+            returns={'uuid': 'e2c25c1b-f6a9-4cf6-b8d2-271e628a9a56'}
+        )
+        self.register_uri(
+            'https://document/service/folder/e2c25c1b-f6a9-4cf6-b8d2-271e628a9a56/children/?page=1&page_size=100&entity_type=file',
+            returns={'next': None, 'results': []}
+        )
+        self.register_uri(
+            'https://document/service/folder/e2c25c1b-f6a9-4cf6-b8d2-271e628a9a56/children/?page=1&page_size=100&entity_type=folder',
+            returns={'next': None, 'results': [{'name': 'folder1'}, {'name': 'folder2'}]}
+        )
+
+        # when
+        file_names = self.client.ls('my_project')
+
+        # then
+        assert_that(file_names, equal_to(['/folder1', '/folder2']))
