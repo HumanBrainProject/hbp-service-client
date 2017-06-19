cb16fef3d2f0101baedd6d1e8491314d63cef271
Allan Francani <allan.francani@epfl.ch>
2017-06-19 17:12:28 +0200

    Test `ls` returns an empty list if project is empty

--- hbp_service_client/document_service/tests/test_highlevel.py
+++ hbp_service_client/document_service/tests/test_highlevel.py
@@ -2,6 +2,9 @@ import unittest
 from mock import Mock
 import httpretty
 import json
+import re
+
+from hamcrest import *
 
 from hbp_service_client.document_service.highlevel import StorageClient
 
@@ -19,9 +22,35 @@ class TestStorageClient(unittest.TestCase):
         httpretty.disable()
         httpretty.reset()
 
+    def register_uri(self, uri, returns):
+        httpretty.register_uri(
+            httpretty.GET, re.compile(re.escape(uri)),
+            match_querystring=True,
+            body=json.dumps(returns),
+            content_type="application/json"
+        )
+
     #
     # ls
     #
 
-    def test_ls(self):
-        return 0
+    def test_ls_should_return_an_empty_list_for_an_empty_project(self):
+        # given
+        self.register_uri(
+            'https://document/service/entity/?path=my_empty_project',
+            returns={'uuid': 'e2c25c1b-f6a9-4cf6-b8d2-271e628a9a56'}
+        )
+        self.register_uri(
+            'https://document/service/folder/e2c25c1b-f6a9-4cf6-b8d2-271e628a9a56/children/?page=1&page_size=100&entity_type=file',
+            returns={'next': None, 'results': []}
+        )
+        self.register_uri(
+            'https://document/service/folder/e2c25c1b-f6a9-4cf6-b8d2-271e628a9a56/children/?page=1&page_size=100&entity_type=folder',
+            returns={'next': None, 'results': []}
+        )
+
+        # when
+        file_names = self.client.ls('my_empty_project')
+
+        # then
+        assert_that(file_names, equal_to([]))
