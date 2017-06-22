191d6087296ae94b33b19690f504497df26e1a9a
Allan Francani <allan.francani@epfl.ch>
2017-06-22 09:21:01 +0200

    Tested and implemented the `method` and `url` methods of the request builder

--- hbp_service_client/request/request_builder.py
+++ hbp_service_client/request/request_builder.py
@@ -1,14 +1,19 @@
+import requests
 from hbp_service_client.document_service.service_locator import ServiceLocator
 
 class RequestBuilder(object):
-    '''A class to make requests'''
+    '''A builder to create requests'''
 
-    def __init__(self, service_locator):
+    def __init__(self, service_locator, method='GET', url='NO_URL'):
         '''
         Args:
            service_locator: collaborator which gets the collab services urls
+           method: the http method (GET, POST...)
+           url: the url to send a request to
         '''
         self._service_locator = service_locator
+        self._method = method
+        self._url = url
 
     @classmethod
     def new(cls, environment='prod'):
@@ -22,3 +27,20 @@ class RequestBuilder(object):
 
         '''
         return cls(ServiceLocator.new(environment))
+
+    def _copy_and_set(self, attribute, value):
+        params = {'method': self._method, 'url': self._url}
+        params[attribute] = value
+        return RequestBuilder(self._service_locator, **params)
+
+    def a_get_request(self):
+        return self._copy_and_set('method', 'GET')
+
+    def to(self, url):
+        return self._copy_and_set('url', url)
+
+    def send(self):
+        return requests.request(
+            self._method,
+            self._url
+        )
--- hbp_service_client/request/tests/test_request_builder.py
+++ hbp_service_client/request/tests/test_request_builder.py
@@ -1,11 +1,35 @@
 import unittest
+import httpretty
+import json
 
-from hbp_service_client.request.request_builder import RequestBuilder
+from hamcrest import *
+
+from hbp_service_client.request.request_builder import RequestBuilder as RequestBuilder
 
 class TestRequestBuilder(unittest.TestCase):
 
     def setUp(self):
+        httpretty.enable()
+        # Fakes the service locator call to the services.json file
+        httpretty.register_uri(
+            httpretty.GET, 'https://collab.humanbrainproject.eu/services.json',
+            body=json.dumps({ 'my_service': {'v3': 'https://my/service/v3'} })
+        )
         self.builder = RequestBuilder.new()
 
-    def test_ready(self):
-        print 'ready!'
+    def tearDown(self):
+        httpretty.disable()
+        httpretty.reset()
+
+    def test_should_send_a_get_request_to_the_given_url(self):
+        # given
+        httpretty.register_uri(
+            httpretty.GET, 'http://a.url',
+            body='the url response'
+        )
+
+        # when
+        response = self.builder.a_get_request().to('http://a.url').send()
+
+        # then
+        assert_that(response.text, equal_to('the url response'))
