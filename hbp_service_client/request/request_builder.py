14ba1e10af1380d005bbb6261a9a880275f25ea4
Allan Francani <allan.francani@epfl.ch>
2017-06-23 12:39:34 +0200

    Tested and implemented the `params` method of the request builder

--- hbp_service_client/request/request_builder.py
+++ hbp_service_client/request/request_builder.py
@@ -4,7 +4,7 @@ from hbp_service_client.document_service.service_locator import ServiceLocator
 class RequestBuilder(object):
     '''A builder to create requests'''
 
-    def __init__(self, service_locator=None, url=None, service_url=None, endpoint=None, headers={}, return_body=False):
+    def __init__(self, service_locator=None, url=None, service_url=None, endpoint=None, headers={}, return_body=False, params={}):
         '''
         Args:
            service_locator: collaborator which gets the collab services urls
@@ -14,6 +14,7 @@ class RequestBuilder(object):
            endpoint: is concatenated to `service_url` to create the url
            headers: headers to add to the request as key/value pairs
            return_body: True if the body of the response should be returned, False if the response should be returned
+           params: params to add to the request as key/value pairs
         '''
         self._service_locator = service_locator
         self._url = url
@@ -21,6 +22,7 @@ class RequestBuilder(object):
         self._endpoint = endpoint
         self._headers = headers
         self._return_body = return_body
+        self._params = params
 
     @classmethod
     def request(cls, environment='prod'):
@@ -42,7 +44,8 @@ class RequestBuilder(object):
             'service_url'     : self._service_url,
             'endpoint'        : self._endpoint,
             'headers'         : self._headers,
-            'return_body'     : self._return_body
+            'return_body'     : self._return_body,
+            'params'          : self._params
         }
         params[attribute] = value
         return RequestBuilder(**params)
@@ -65,6 +68,11 @@ class RequestBuilder(object):
     def with_token(self, token):
         return self.with_headers({'Authorization': 'Bearer {}'.format(token)})
 
+    def with_params(self, params):
+        copy = params.copy()
+        copy.update(self._params)
+        return self._copy_and_set('params', copy)
+
     def return_body(self):
         return self._copy_and_set('return_body', True)
 
@@ -85,12 +93,13 @@ class RequestBuilder(object):
         response = requests.request(
             method,
             url,
-            headers=self._headers
+            headers=self._headers,
+            params=self._params
         )
 
         if self._return_body:
              return self._extract_body(response)
-             
+
         return response
 
     def _extract_body(self, response):
