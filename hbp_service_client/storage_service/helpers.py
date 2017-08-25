9c918f644bc933ec173ea3015f41a5edb847f88c
Akos Hencz <akos.hencz@epfl.ch>
2017-08-25 10:58:58 +0200

    Remove unsused helpers module

--- hbp_service_client/storage_service/helpers.py
+++
@@ -1,9 +0,0 @@
-'''helper functions for initializing Storage client on the desktop or collab Jupyter notebooks'''
-
-import hbp_service_client.storage_service.client import StorageClient
-
-def desktop_get_storage_client(bearer_token):
-    return StorageClient(bearer_token)
-
-def collab_get_storage_client():
-    return desktop_get_storage_client(oauth.get_token())
